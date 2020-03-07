cdaef11b5f3703156e76df279c0e2641c4602ab9
William Pei <william.h.pei@gmail.com>
2020-03-07 14:24:23 +1100

    Make it look pretty

--- assignments/a1/a1.py
+++ assignments/a1/a1.py
@@ -25,17 +25,16 @@ def get_top_stems(document, n, stopwords):
     >>> get_top_stems(emma, 10, my_stopwords)
     ['mr.', "'s", 'emma', 'could', 'would', 'mrs.', 'miss', 'must', 'harriet', 'much']
     """
+    # initialise our stemmer and extract all words from the document
     porter = PorterStemmer()
     words = word_tokenize(document)
-
+    # convert stopwords to lower case, then filter words
     lower_case_stopwords = [w.lower() for w in stopwords]
-    filtered_words = [
-        w for w in words if w.lower() not in lower_case_stopwords]
-
-    stems = []
-    for word in filtered_words:
-        stems.append(porter.stem(word))
-
+    filtered_words = [w for w in words
+                      if w.lower() not in lower_case_stopwords]
+    # get the stems from filtered_words
+    stems = [porter.stem(word) for word in filtered_words]
+    # get the frequency distribution of stems so that we can sort it
     stem_frequency = nltk.FreqDist(stems)
     stem_frequency_list = list(stem_frequency.items())
     stem_frequency_list.sort(key=operator.itemgetter(1), reverse=True)
@@ -56,14 +55,13 @@ def sentence_to_set(sentence, list_of_stems):
     >>> sentence_to_set(long_sentence, ["seem", "chapter", "emma", "the"]) == {'the', 'emma', 'seem'}
     True
     """
+    # intialise our stemmer and extract words from sentence
     porter = PorterStemmer()
     words = word_tokenize(sentence)
-
+    # convert our list of stems to lower case and stem our words
     lower_case_list_of_stems = [s.lower() for s in list_of_stems]
-    stems = []
-    for word in words:
-        stems.append(porter.stem(word).lower())
-
+    stems = [porter.stem(word).lower() for word in words]
+    # filter our list of stems
     filtered_stems = [s for s in stems if s in lower_case_list_of_stems]
 
     return set(filtered_stems)
@@ -125,34 +123,39 @@ def get_transition_matrix(list_sentences, list_of_stems, threshold=0.5):
     j = 0
     arr = []
 
+    # loop through the list of sentences and compare each sentence
+    # to every other sentence including itself in order to calculate
+    # the jaccard score
     while i < len(list_sentences):
         linked = np.array([])
         while j < len(list_sentences):
-            # if i == j:
-            #     linked = np.append(linked, 0)
-            # else:
-            jaccard_score = compute_jaccard(
-                list_sentences[i], list_sentences[j], list_of_stems)
+            jaccard_score = compute_jaccard(list_sentences[i],
+                                            list_sentences[j],
+                                            list_of_stems)
 
-            if jaccard_score >= threshold:
-                linked = np.append(linked, 1.)
-            else:
-                linked = np.append(linked, 0)
+            # record a 1 in the array if the jaccard score crosses
+            # the threshold, otherwise record a zero
+            meets_threshold = jaccard_score >= threshold
+            linked = (np.append(linked, 1)
+                      if meets_threshold
+                      else np.append(linked, 0))
             j += 1
+
+        # count how many links we have (ones) in the linked array
+        # then divide each link by this count if count is not 0
         counts = Counter(linked)
-        if counts[1.] == 0:
-            scaled_links = [1/len(linked) for x in linked]
-        else:
-            scaled_links = [x/counts[1.] for x in linked]
-        if len(arr) == 0:
-            arr = np.array([scaled_links])
-        else:
-            arr = np.append(arr, [scaled_links], axis=0)
+        scaled_links = ([1/len(linked) for x in linked]
+                        if counts[1.] == 0
+                        else [x/counts[1.] for x in linked])
+
+        arr = (np.array([scaled_links])
+               if len(arr) == 0
+               else np.append(arr, [scaled_links], axis=0))
         i += 1
         j = 0
 
-    transition_matrix = arr.transpose()
-    return transition_matrix
+    # transpose the array to get our transition_matrix
+    return arr.transpose()
 
 # Task 4 (1 mark)
 
@@ -177,8 +180,8 @@ def compute_pagerank(list_sentences, list_of_stems, threshold=0.5, damping_facto
     """
     num_sentences = len(list_sentences)
     A = get_transition_matrix(list_sentences, list_of_stems, threshold)
-    M = damping_factor * A + (1 - damping_factor) * \
-        (1./num_sentences * np.ones((num_sentences, num_sentences)))
+    M = (damping_factor * A + (1 - damping_factor) *
+         (1./num_sentences * np.ones((num_sentences, num_sentences))))
 
     iterations = 0
     PR = 1./num_sentences * np.ones((num_sentences, 1))
@@ -221,17 +224,22 @@ def summarise(text, list_of_stems, N=3, threshold=0.5, damping_factor=0.85, epsy
     It was Miss
     Taylor's loss which first brought grief.
     """
+    # extract sentences from text
     sents = nltk.sent_tokenize(text)
+    # calculate the page rank
     PR = compute_pagerank(sents, list_of_stems, threshold,
                           damping_factor, epsylon)
+    # map the ranks into a 2d list so that we can sort it
     rankings = map(lambda x, y: [x, y[0]], sents, PR)
     unsorted_rankings_list = list(rankings)
     sorted_rankings_list = [x for x in unsorted_rankings_list]
-    sorted_rankings_list.sort(key=lambda x: x[1])
+    # sort on the page rank column
+    sorted_rankings_list.sort(key=lambda x: x[1], reverse=True)
+    # get the N highest ranking sentences
     highest_sents = []
     for i in range(N):
-        j = (i + 1) * -1
-        highest_sents.append(sorted_rankings_list[j][0])
+        highest_sents.append(sorted_rankings_list[i][0])
+    # intersect the two arrays to get our matching sentences
     intersect = [sentence for sentence in sents if sentence in highest_sents]
     return intersect
 
